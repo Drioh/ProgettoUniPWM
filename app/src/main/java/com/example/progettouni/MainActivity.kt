b83bcf23ec21e72a0d055aa62cc3e5877c0ad046
Haydnz <96525793+Haydnz@users.noreply.github.com>
2023-06-13 18:05:20 +0200

    db connessione v1

--- app/src/main/java/com/example/progettouni/MainActivity.kt
+++ app/src/main/java/com/example/progettouni/MainActivity.kt
@@ -4,17 +4,26 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import api.ApiService
 import com.example.progettouni.databinding.ActivityMainBinding
 import com.example.progettouni.databinding.RealAppBinding
 import fragment_classes.Home
 import fragment_classes.RealApp
 import fragment_classes.SubscriptionPurchase
 import fragment_classes.UserOrAdmin
-
+import com.google.gson.JsonObject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Call
+import retrofit2.Callback
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var realBinding: RealAppBinding
     private lateinit var log_type: String
+    private val apiService = ApiService()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -46,19 +55,67 @@ class MainActivity : AppCompatActivity() {
     fun backTo(tag: String){
         supportFragmentManager.popBackStack(tag,0)
     }
-    fun loginCheck(mail: String, password: String){
-        if(mail== "cazzi" && password == "cazzi"){
-            realBinding = RealAppBinding.inflate(layoutInflater)
-            supportFragmentManager.popBackStack()
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainerView, RealApp())
-                .commit()
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragmentContainerView4, Home())
-                .addToBackStack("Home")
-                .commit()
 
+    fun loginCheck(mail: String, password: String) {
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                val utenti = apiService.getUtenti()
+                println("Utenti nel database:")
+                utenti?.forEach {
+                    println("ID: ${it.id}")
+                    println("Mail: ${it.mail}")
+                    println("Nome: ${it.nome}")
+                    println("Cognome: ${it.cognome}")
+                    println("Password: ${it.password}")
+                    println("Profilo: ${it.propic}")
+                    println("Codice Verifica: ${it.cod_ver}")
+                    println("Verificato: ${it.verificato}")
+                    println("------------------------------")
+                }
+                if (!utenti.isNullOrEmpty()) {
+                    val utenteTrovato = utenti.find { it.mail == mail && it.password == password }
+                    if (utenteTrovato != null) {
+                        // Utente trovato nel database
+                        // Esegui l'azione di navigazione verso l'area riservata all'utente
+                        realBinding = RealAppBinding.inflate(layoutInflater)
+                        supportFragmentManager.popBackStack()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.fragmentContainerView, RealApp())
+                            .commit()
+                        supportFragmentManager.beginTransaction()
+                            .add(R.id.fragmentContainerView4, Home())
+                            .addToBackStack("Home")
+                            .commit()
+                    } else {
+                        // Utente non trovato o credenziali errate
+                        // Gestisci il caso di autenticazione fallita
+                        handleAuthenticationFailure()
+                    }
+                } else {
+                    // Nessun utente nel database
+                    // Gestisci il caso di autenticazione fallita
+                    handleAuthenticationFailure()
+                }
+            } catch (e: Exception) {
+                // Gestisci l'errore di rete o altre eccezioni
+                handleNetworkFailure()
+            }
         }
+    }
+
+    private fun handleAuthenticationFailure() {
+        Toast.makeText(this, "Autenticazione fallita. Credenziali errate.", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun handleQueryFailure() {
+        Toast.makeText(this, "Errore nella risposta della query.", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun handleNetworkFailure() {
+        Toast.makeText(this, "Errore di rete o altre eccezioni.", Toast.LENGTH_SHORT).show()
+
     }
     fun subChoice(teatro: String){
         supportFragmentManager.beginTransaction()
--- app/src/main/java/fragment_classes/Login.kt
+++ app/src/main/java/fragment_classes/Login.kt
@@ -2,16 +2,24 @@ package fragment_classes
 
 import android.graphics.Color
 import android.os.Bundle
+import android.telecom.Call
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import api.ApiService
 import com.example.progettouni.MainActivity
 import com.example.progettouni.R
 import com.example.progettouni.databinding.FragmentLoginBinding
+import com.google.gson.JsonObject
+import okhttp3.Response
+import javax.security.auth.callback.Callback
+
 
 class Login : Fragment(R.layout.fragment_login) {
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var userApi: ApiService
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,14 +28,14 @@ class Login : Fragment(R.layout.fragment_login) {
         binding = FragmentLoginBinding.inflate(inflater)
         var MA = (activity as MainActivity?)!!
 
-        binding.cancelButton.setOnClickListener{
+        binding.cancelButton.setOnClickListener {
             binding.cancelButton.setBackgroundColor(Color.parseColor("#F44336"))
             MA.back()
 
         }
-        binding.confirmButton.setOnClickListener{
+        binding.confirmButton.setOnClickListener {
             binding.confirmButton.setBackgroundColor(Color.parseColor("#F44336"))
-            MA.loginCheck(binding.mailField.text.toString(),binding.pwField.text.toString())
+            MA.loginCheck(binding.mailField.text.toString(), binding.pwField.text.toString())
 
         }
         binding.registerButton.setOnClickListener {
