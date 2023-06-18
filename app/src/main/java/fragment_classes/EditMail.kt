4a08d993cb0f23b58e87f70c270041ef81142ccb
Haydnz <96525793+Haydnz@users.noreply.github.com>
2023-06-18 18:43:26 +0200

    db connessione v6

--- app/src/main/java/fragment_classes/EditMail.kt
+++ app/src/main/java/fragment_classes/EditMail.kt
@@ -2,18 +2,24 @@ package fragment_classes
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import api.ApiService
 import com.example.progettouni.MainActivity
 import com.example.progettouni.R
 import com.example.progettouni.databinding.FragmentEditMailBinding
 import com.example.progettouni.databinding.FragmentEditPasswordBinding
 import com.example.progettouni.databinding.FragmentRetrievePasswordBinding
 import com.example.progettouni.databinding.FragmentSubscriptionPurchaseBinding
-
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 class EditMail: Fragment(R.layout.fragment_retrieve_password){
     private lateinit var binding: FragmentEditMailBinding
     override fun onCreateView(
@@ -28,6 +34,8 @@ class EditMail: Fragment(R.layout.fragment_retrieve_password){
             binding.confirmButton.setBackgroundColor(Color.parseColor("#F44336"))
             /*viene effettutato un test sulla mail, se la vecchia mail corrisponde con quella
             presente nel database allora verrà permesso all'utente di modificarla*/
+
+            changeEmail(binding.cardField.text.toString(),binding.cardOwnerField.text.toString())
             MA.showToast("Mail modificata correttamente")
             MA.back()
         }
@@ -38,4 +46,48 @@ class EditMail: Fragment(R.layout.fragment_retrieve_password){
 
         return binding.root
     }
+
+
+    fun changeEmail(oldEmail: String, newEmail: String) {
+        val query = "SELECT * FROM Utente WHERE mail = '${oldEmail}';"
+        ApiService.retrofit.select(query).enqueue(object : Callback<JsonObject> {
+            override fun onResponse(call: Call<JsonObject>?, response: Response<JsonObject>) {
+                if (response.isSuccessful) {
+                    val jsonArray = response.body()?.getAsJsonArray("queryset")
+                    if (jsonArray?.size() == 1) {
+                        val utenteId = jsonArray[0].asJsonObject["id_utente"].asInt
+                        updateEmailInDatabase(utenteId, newEmail)
+                    } else {
+                        Log.i("ApiService","Vecchia email non valida")
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                Log.e("Errore di rete",t.message.toString())
+            }
+        })
+    }
+
+    fun updateEmailInDatabase(utenteId: Int?, newEmail: String) {
+        if (utenteId != null) {
+            val query = "UPDATE Utente SET mail = '${newEmail}' WHERE id_utente = ${utenteId};"
+            ApiService.retrofit.update(query).enqueue(object : Callback<JsonObject> {
+                override fun onResponse(call: Call<JsonObject>?, response: Response<JsonObject>) {
+                    if (response.isSuccessful) {
+                        Log.i("ApiService","Email modificata correttamente")
+                    } else {
+                        Log.i("ApiService","Errore durante la modifica dell'email")
+                    }
+                }
+
+                override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                    Log.e("Errore di rete", t.message.toString())
+                }
+            })
+        }
+    }
+
+
+
 }
--- app/src/main/java/fragment_classes/EditPassword.kt
+++ app/src/main/java/fragment_classes/EditPassword.kt
@@ -2,16 +2,22 @@ package fragment_classes
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import api.ApiService
 import com.example.progettouni.MainActivity
 import com.example.progettouni.R
 import com.example.progettouni.databinding.FragmentEditPasswordBinding
 import com.example.progettouni.databinding.FragmentRetrievePasswordBinding
 import com.example.progettouni.databinding.FragmentSubscriptionPurchaseBinding
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class EditPassword: Fragment(R.layout.fragment_retrieve_password){
     private lateinit var binding: FragmentEditPasswordBinding
@@ -27,6 +33,7 @@ class EditPassword: Fragment(R.layout.fragment_retrieve_password){
             binding.confirmButton.setBackgroundColor(Color.parseColor("#F44336"))
             /*viene effettutato un test sulla mail, se la mail corrisponde con quella nel database
             allora verrà permesso all'utente di modificare la password*/
+            changePassword(binding.cardField.text.toString(), binding.cardOwnerField.text.toString())
             MA.showToast("Password modificata correttamente")
             MA.back()
         }
@@ -37,4 +44,45 @@ class EditPassword: Fragment(R.layout.fragment_retrieve_password){
 
         return binding.root
     }
+
+
+    fun changePassword(password: String, newPassword: String) {
+        val query = "SELECT * FROM Utente WHERE password = '${password}';"
+        ApiService.retrofit.select(query).enqueue(object : Callback<JsonObject> {
+            override fun onResponse(call: Call<JsonObject>?, response: Response<JsonObject>) {
+                if (response.isSuccessful) {
+                    val jsonArray = response.body()?.getAsJsonArray("queryset")
+                    if (jsonArray?.size() == 1) {
+                        val utenteId = jsonArray[0].asJsonObject["id_utente"].asInt
+                        updatePasswordInDatabase(utenteId, newPassword)
+                    } else {
+                        Log.i("ApiService"," password non valida")
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                Log.e("Errore di rete",t.message.toString())
+            }
+        })
+    }
+    fun updatePasswordInDatabase(utenteId: Int?, newPassword: String) {
+        if (utenteId != null) {
+            val query = "UPDATE Utente SET password = '${newPassword}' WHERE id_utente = ${utenteId};"
+            ApiService.retrofit.update(query).enqueue(object : Callback<JsonObject> {
+                override fun onResponse(call: Call<JsonObject>?, response: Response<JsonObject>) {
+                    if (response.isSuccessful) {
+                        Log.i("ApiService","Password modificata correttamente")
+                    } else {
+                        Log.i("ApiService","Errore durante la modifica della password")
+                    }
+                }
+
+                override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                    Log.e("Errore di rete", t.message.toString())
+                }
+            })
+        }
+    }
+
 }
