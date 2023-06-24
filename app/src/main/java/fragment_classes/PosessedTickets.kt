445916675601173a4ee9b49e07f7d770237d6b53
Samuele <samueleferruggia@virgilio.it>
2023-06-24 22:08:40 +0200

    aggiunto qr code

--- app/src/main/java/fragment_classes/PosessedTickets.kt
+++ app/src/main/java/fragment_classes/PosessedTickets.kt
@@ -14,7 +14,7 @@ class PosessedTickets : Fragment(R.layout.fragment_posessed_tickets) {
     private lateinit var binding: FragmentPosessedTicketsBinding
     private lateinit var type: String
     private lateinit var period: String
-    private lateinit var essence: ArrayList<Boolean>     //mi indica se lavoro con un biglietto singolo o un abbonamento
+    private lateinit var id_ticket: String
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,24 +25,36 @@ class PosessedTickets : Fragment(R.layout.fragment_posessed_tickets) {
         var MA = (activity as MainActivity?)!! //reference alla Main Activity
         binding.ticketRecycler.layoutManager = LinearLayoutManager(this.context)
         val data = ArrayList<TicketModel>()
+        val essence = ArrayList<Boolean>()     //mi indica se lavoro con un biglietto singolo o un abbonamento
+        val allId = ArrayList<String>()    //mi crea una lista di id
         //for (i in 1..20) {     //dovrei fare in modo di fare un while per scorrermi tutte le tuple del dbms
         type = "abbonamento"
         period = "6 mesi"
+        id_ticket = "xlcsbm"    //devo prendermi dal database anche id del biglietto/abbonamento
         data.add(TicketModel(R.drawable.ticket_icon_white, type, period))
         if("abbonamento" == type){
-            essence.add(true)                                   //se il biglietto cliccato è un abbonamento allora essence è true
-        }
+            essence.add(true)                           //se il biglietto cliccato è un abbonamento allora essence è true
+            id_ticket = "abbonamento$id_ticket"
+        } else {essence.add(false)}
+        allId.add(id_ticket)
 
         type = "Orlando Innamorato"
         period = "3 mesi"
+        id_ticket = "jtrnnyj"
         data.add(TicketModel(R.drawable.ticket_icon_white, type, period))     //type e period sono i valori che dovrebbero essere presi dal dbms
+        if("abbonamento" == type){
+            essence.add(true)
+            id_ticket = "abbonamento$id_ticket"
+        } else {essence.add(false)}
+        allId.add(id_ticket)
+
         //}
         val adapter = TicketAdapter(data)                          //importante creare l'adapter dopo gli add sennò viene passato un ArrayList vuoto
         binding.ticketRecycler.adapter = adapter
 
         adapter.setOnClickListener(object: TicketAdapter.OnClickListener {
             override fun onClick(position: Int, model: TicketModel) {
-                MA.realAppNavigateTo(Ticket(essence, position), "ticket")
+                MA.realAppNavigateTo(Ticket(essence[position], allId[position]), "ticket")
             }
         })
         return binding.root
--- app/src/main/java/fragment_classes/Ticket.kt
+++ app/src/main/java/fragment_classes/Ticket.kt
@@ -1,33 +1,78 @@
 package fragment_classes
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.progettouni.MainActivity
 import com.example.progettouni.R
 import com.example.progettouni.databinding.FragmentTicketBinding
+import java.util.*
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.qrcode.QRCodeWriter
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
 
-class Ticket(val essence: ArrayList<Boolean>, position: Int): Fragment(R.layout.fragment_ticket) {
+class Ticket(private val essence: Boolean, id: String): Fragment(R.layout.fragment_ticket) {
     private lateinit var binding: FragmentTicketBinding
     private lateinit var button: Button
-    private var position: Int = position
+    private lateinit var text: TextView
+    private lateinit var image: ImageView
+    private var id = id
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         binding = FragmentTicketBinding.inflate(inflater)
         var MA = (activity as MainActivity?)!! //reference alla Main Activity
         button = binding.showInfoButton
+        text = binding.textView
+        image = binding.imageView
 
-        if(essence.get(position)){
-            button.setText("visualizza info abbonamento")
+        generateQRCode(id)
+
+        if(essence){
+            button.setText(R.string.infoMembership)
+            text.setText(R.string.myMembership)
+            //("inserire pagina di informazioni abbonamento")
         }
-
+        //("inserire pagina di informazioni biglietto")
 
         return binding.root
+    }
+
+    private fun generateQRCode(data: String) {
+        try {
+            // Set QR code parameters
+            val hints = Hashtable<EncodeHintType, Any>()
+            hints[EncodeHintType.CHARACTER_SET] = "UTF-8"
+            hints[EncodeHintType.ERROR_CORRECTION] = ErrorCorrectionLevel.H
+            hints[EncodeHintType.MARGIN] = 2
+
+            // Create QR code writer and encode the data
+            val qrCodeWriter = QRCodeWriter()
+            val bitMatrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, 512, 512, hints)
+
+            // Convert bit matrix to bitmap
+            val width = bitMatrix.width
+            val height = bitMatrix.height
+            val bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    bmp.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+                }
+            }
+            image.setImageBitmap(bmp)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
