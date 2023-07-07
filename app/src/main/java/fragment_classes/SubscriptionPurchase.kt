f0c28877aff6816c2a64c116e8f7a84d22111afa
Samuele <samueleferruggia@virgilio.it>
2023-07-08 00:56:00 +0200

    problemi con ticketQuantity

--- app/src/main/java/fragment_classes/SubscriptionPurchase.kt
+++ app/src/main/java/fragment_classes/SubscriptionPurchase.kt
@@ -2,16 +2,23 @@ package fragment_classes
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import api.ApiService
 import com.example.progettouni.MainActivity
 import com.example.progettouni.R
 import com.example.progettouni.databinding.FragmentSubscriptionPurchaseBinding
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.time.LocalDate
 import java.time.Period
+import java.util.Calendar
 
 class SubscriptionPurchase(var theatre: String) : Fragment(R.layout.fragment_subscription_purchase) {
     private lateinit var binding: FragmentSubscriptionPurchaseBinding
@@ -42,12 +49,36 @@ class SubscriptionPurchase(var theatre: String) : Fragment(R.layout.fragment_sub
             period = 12
         }
         binding.confirmButton.setOnClickListener(){
+
+            var cardNumber = binding.cardField.text.toString()
+            var numberCVC = binding.cvcField.text.toString()
+            var expireYear = binding.cardExpireYearField.text.toString()
+            var expireMonth = binding.cardExpireMonthField.text.toString()
+
+            expireYear = adjustYear(expireYear)
+
+            //scelgo di non accorpare gli if perché voglio prima verificare il selectedButton e in caso passare al suo else e poi andare con l'altro
             if(selectedbutton!=null) {
-                var currentDate = LocalDate.now()
-                var subLength = Period.of(0, period, 0)
-                var expireDate = currentDate.plus(subLength)
-                MA.realAppNavigateTo(PaymentConfirmed("Abbonamento"), "ConfirmedPayment")
-            }else{
+                if((cardNumber.length == 16) && (numberCVC.length == 3) && verifyExpire(expireYear, expireMonth)){
+
+                    var currentDate = LocalDate.now()
+                    var subLength = Period.of(0, period, 0)   //inserisco 1, 3, 6 o 12 mesi al giorno corrente
+                    var lastMembershipDate = currentDate.plus(subLength)
+
+                    var utente = MA.getUserId()
+
+                    if(expireMonth.length == 1) {    //quindi se non inserisco il "20" prima dell'anno
+                        expireMonth = "0${expireMonth}"
+                    }
+                    insertCartaCredito(utente, cardNumber, numberCVC, expireYear, expireMonth)
+                    insertAbbonamentoInRemoto(theatre, utente, period, lastMembershipDate)
+                    MA.syncDB()
+                    MA.realAppNavigateTo(PaymentConfirmed("Abbonamento"), "ConfirmedPayment")
+                }
+                else {
+                    MA.showToast("Carta non valida")
+                }
+            }else {
                 MA.showToast("Selezionare prima un periodo di validità")
             }
         }
@@ -57,6 +88,24 @@ class SubscriptionPurchase(var theatre: String) : Fragment(R.layout.fragment_sub
 
         return binding.root
     }
+
+    private fun insertAbbonamentoInRemoto(theatre: String, utente: Int, period: Int, lastMembershipDate: LocalDate?) {
+        val query = "insert into Abbonamento (ref_teatro, ref_utente, durata_mesi, data_scadenza ) values ('${theatre}', '${utente}, '${period}, '${lastMembershipDate}'); "
+        ApiService.retrofit.insert(query).enqueue(
+            object: Callback<JsonObject> {
+                override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                    println(response.code())
+                    Log.i("ApiService", "Inserimento avvenuto correttamente!")
+                }
+                override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                    Log.i("ApiService", "Pagamento fallito")
+                    Log.e("ApiService", t.message.toString())
+
+                }
+            }
+        )
+    }
+
     private fun selectButton(button: Button){
         if(selectedbutton == null){
             button.setBackgroundColor(Color.parseColor("#F44336"))
@@ -67,4 +116,47 @@ class SubscriptionPurchase(var theatre: String) : Fragment(R.layout.fragment_sub
             button.setBackgroundColor(Color.parseColor("#F44336"))
         }
     }
+    private fun adjustYear(expireYear: String): String {
+        var year: Int = Integer.parseInt(expireYear)
+        if(year < 100){    //quindi se non inserisco il "20" prima dell'anno
+            year += 2000
+        }
+        return year.toString()
+    }
+
+    private fun verifyExpire(year: String, month: String): Boolean {
+        var expireYear: Int = Integer.parseInt(year)
+        var expireMonth: Int = Integer.parseInt(month)
+        val current_year = Calendar.getInstance().get(Calendar.YEAR)
+        val current_month = Calendar.getInstance().get(Calendar.MONTH)
+        if(expireMonth > 12){
+            return false
+        }
+        if(expireYear-current_year > 0){
+            return true
+        }
+        else if((expireYear-current_year == 0) && (expireMonth-current_month >= 0)){
+            return true
+        }
+        return false
+    }
+
+    private fun insertCartaCredito(utente: Int, cardNumber: String, numberCVC: String, expireYear: String, expireMonth: String) {
+        var date = LocalDate.parse("${expireYear}-${expireMonth}-01")
+        val query = "insert into Carte_credito (numero, ref_utente, data_scadenza_carta, cvc ) values ('${cardNumber}', '${utente}', '${date}', '${numberCVC}'); "
+        ApiService.retrofit.insert(query).enqueue(
+            object: Callback<JsonObject> {
+                override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                    println(response.code())
+                    Log.i("ApiService", "Inserimento avvenuto correttamente!")
+                }
+                override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                    Log.i("ApiService", "Inserimento fallito")
+                    Log.e("ApiService", t.message.toString())
+
+                }
+            }
+        )
+    }
+
 }
